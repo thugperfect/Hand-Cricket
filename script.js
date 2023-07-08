fa1f100ac16758bd6034d4e8b91dce671dae5220
thugperfect <meenamani9444@gmail.com>
2023-07-08 10:10:58 +0000

    added right side

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ function start(){
     startBtn.style.display = "none"
     let body = document.getElementById("body")
     let left = document.createElement("div")
-    left.className = "left"
+    left.id = "left"
     let middle = document.createElement("div")
-    middle.className = "middle"
+    middle.id= "middle"
     let right = document.createElement("div")
-    right.className = "right"
+    right.id = "right"
     body.appendChild(left)
     body.appendChild(middle)
     body.appendChild(right)
+    rightfn()
+
+
+}
+
+function rightfn(){
+    
+    let rightel = document.getElementById("right")
+    let displayBox = document.createElement("div")
+    displayBox.id = "display-box"
+
+
+    let optionBox = document.createElement("div")
+    optionBox.id = "option-box"
+    rightel.appendChild(displayBox)
+    rightel.appendChild(optionBox)
+    
+}
+
+
+function toss(){
+    let oddOrEven = Math.floor(Math.random()*7)
 
 }
