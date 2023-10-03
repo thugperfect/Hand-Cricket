4fb237116842b5b3e239163da40462db2c73ecca
thugperfect <meenamani9444@gmail.com>
2023-10-03 15:34:48 +0530

    initial structure

---
+++ script.js
@@ -0,0 +1,8 @@
+const start_btn = document.getElementById('start')
+
+const game_page = document.getElementById('game')
+
+function start(){
+  start_btn.style.display = 'none'
+  game_page.style.display = 'block'
+}
