7dadb843f11c6ca01371d78c031fd294e344d278
thugperfect <meenamani9444@gmail.com>
2023-10-05 20:27:13 +0530

    fn to find click

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const right = document.getElementById('right')
 const left =  document.getElementById('left')
 const center = document.getElementById('center')
 let toss = false
+let tossVal = ''
 let scoreRight = 0
 let scoreLeft = 0
 function start(){
@@ -18,12 +19,31 @@ if(!toss){
   tossBtn.innerText = 'TOSS'
   tossDiv.appendChild(tossBtn)
   center.appendChild(tossDiv)
+  tossBtn.onclick = function fun(){
+    tossBtn.style.display = 'none'
+    const odd = document.createElement('button')
+    const even = document.createElement('button')
+    odd.innerText = 'odd'
+    
+    even.innerText = 'even'
+    odd.className = 'oddeven'
+    even.className = 'oddeven'
 
+    const oddeven = document.getElementsByClassName('oddeven')
+    for(i = 0;i<oddeven.length;i++){
+      oddeven[i].addEventListener('click',()=>{
+        tossVal = oddeven[i].innerText
+        tossDiv.style.display = 'none'
+        console.log(tossVal)
+      })
+    }
+    tossDiv.appendChild(odd)
+    tossDiv.appendChild(even)
+  }
+  
 }
 
-
 const base_panel = document.getElementById('base')
-
 for(i = 1;i<7;i++){
   const click_btn = document.createElement('div')
   click_btn.className = 'click-btn'
