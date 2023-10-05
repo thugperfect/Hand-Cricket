d1fd10c2fba17f83c6c0aef900f66feefbbd9400
thugperfect <meenamani9444@gmail.com>
2023-10-05 20:15:43 +0530

    if not toss

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const start_btn = document.getElementById('start')
 const game_page = document.getElementById('game')
 const right = document.getElementById('right')
 const left =  document.getElementById('left')
-
+const center = document.getElementById('center')
 let toss = false
 let scoreRight = 0
 let scoreLeft = 0
@@ -10,6 +10,17 @@ function start(){
   start_btn.style.display = 'none'
   game_page.style.display = 'block'
 }
+
+if(!toss){
+  const tossDiv = document.createElement('div')
+  tossDiv.className = 'center'
+  const tossBtn = document.createElement('button')
+  tossBtn.innerText = 'TOSS'
+  tossDiv.appendChild(tossBtn)
+  center.appendChild(tossDiv)
+
+}
+
 
 const base_panel = document.getElementById('base')
 
